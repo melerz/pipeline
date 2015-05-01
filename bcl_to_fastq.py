4912eda17a3027e7d1c7e554034b9df18f3de41a
Guy Meler <melguy@gmail.com>
2015-05-01 08:28:24 -0400

    Skeleton

---
+++ bcl_to_fastq.py
@@ -0,0 +1,46 @@
+import logging
+import os
+from lib import createfastq_operations as operations
+import sys
+import requests
+
+logger = logging.getLogger(__name__) #need to change that to __name__
+def run(data):
+	'''
+		For each expierment in the data, create a folder with the expirement name, and init it by
+		calling the createRundir function.
+
+		data is a JSON forematted data, that describe the current run (run->expierments->(configuration,samples))
+
+		This method first calls to createRundir (links, copy the RunInfo.xml),
+		and then, for each experiment it calls for runExpirement method
+	'''
+	try:
+		#This logger object is used in the library too
+
+		logger.info(("Currently experiment:{0}".format(data['name'])))
+		
+		#Save the current location because createRunDir change it
+		currentLocation=os.getcwd()
+
+		logger.info("{0}: Init Directory".format(data['name']))
+		#Init experiment folder (Link files, RunInfo.xml, create output directory)
+		operations.createRundir(data)
+
+		logger.info("{0}: Run bcl2fastq".format(data['name']))
+		#running bcl2fastq in the current experiment folder.
+		#Output folder is fastq, created by createRundir
+		operations.runExpirement(data)
+
+		# update_data(settings.JOB_ENDPOINT+"%s/"%data['job_id'],
+		# 		{'status':'Finished','description':'Succefully Finished to generate fastq'})
+		# #Changing back to the main folder
+
+		os.chdir(currentLocation)
+
+		logger.info("{0}: Finished".format(data['name']))
+
+	except Exception as e:
+		# update_data(settings.JOB_ENDPOINT+"%s/"%data['job_id'],
+		# 	{'status':'Failed','description':'%s'%e})
+		raise Exception("Error in bcl_to_fastq.py: %s",e)
---
+++ pipeline.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import sys
+import json
+import importlib
+def run(log_level="INFO",log_file="./fastq-log.log",config_file="./config.json"):
+
+	try:
+		logger = configure_logging(log_level,log_file)
+		logger.info("loading config file...")
+		config = json.load(open(config_file))
+		workflow = config['workflows']['fastq_only']
+		for step in workflow['pipeline']:
+			logger.info(("Currently step:{0}".format(step))
+			step_module=importlib.import_module(step)
+			step_module.run(config['data'])
+		logger.info("Finished executing pipeline!")
+
+	except Exception as e:
+		exc_type,exc_obj,exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+		print(exc_type,fname,exc_tb.tb_lineno)
+		if logger:
+			logger.exception("an error has been occured: %s"%e)
+		else:
+			print "main exception. See log file for further details:%s"%e
+
+
+
+def configure_logging(log_level="INFO",log_file="./fastq-log.log"):
+	loglevel = getattr(logging,log_level.upper(),None)
+	if not isinstance(loglevel, int):
+		raise Exception("Invalid log level: %s" %loglevel)
+	#logging.basicConfig(filename=log_file,level=loglevel,foremat="%(name)s:%(levelname)s:%(message)s")
+	logger=logging.getLogger(__name__) #need to change that to __name__
+	logger.setLevel(loglevel)
+	file_handler = logging.FileHandler(log_file)
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	file_handler.setFormatter(formatter)
+	logger.addHandler(file_handler)
+	return logger
+
+
+if __name__ = "main":
+	run(log_level="INFO",log_file="./fastq-log.log",config_file="./config.json")
