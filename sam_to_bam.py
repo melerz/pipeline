a15206c5530aebef4edf279c4190b0200f761d41
Guy Meler <melguy@gmail.com>
2015-05-20 05:39:26 -0400

    bam workflow

--- sam_to_bam.py
+++ sam_to_bam.py
@@ -13,11 +13,39 @@ def run(config_file="./config.json"):
 		print "Running samtools..."
 		config = json.load(open(config_file))
 		logger.info("Loading Config files...")
-		samtools_exec = config['settings']['tools']['samtools']['exec']
+		working_dir = config['settings']['WORKING_DIR']+config['data']['url']
+		bowtie_dir = config['settings']['BOWTIE_OUTPUT_DIR']
 		bam_dir = config['settings']['BAM_OUTPUT_DIR']
-		cmd="%s view -bS %s > %s.bam"
-	except Exception, e:
-		exc_type,exc_obj,exc_tb = sys.exc_info()
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		os.chdir(currentLocation)
+		samtools_exec = config['settings']['tools']['samtools']['exec']
+
+		bowtie_full_path=os.path.join(working_dir,bowtie_dir)
+		bam_full_path=os.path.join(working_dir,bam_dir)
+
+
+		create_bam(bowtie_path=bowtie_full_path,output="bam_full_path",exec_path=samtools_exec)
+
+except Exception, e:
+	exc_type,exc_obj,exc_tb = sys.exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	os.chdir(currentLocation)
 		raise Exception("Error in sam_to_bam: %s,%s,%s,%s"%(e,exc_type,exc_obj,fname))
+
+
+def create_bam(bowtie_path,output="./bam_files/",exec_path="/cs/wetlab/pipeline/samtools/bin/samtools"):
+		logger.debug("create_bam:START")
+		logger.debug("create_bam: changing dir: %s"%bowtie_path)
+		currentLocation = os.getcwd()
+		os.chdir(bowtie_path)
+		cmd="%s view -bS %s > %s.bam"
+		if not (os.path.isdir(output)):
+			os.mkdir(output)
+		for bowtie_file in glob.glob("*"):
+			cmd = [exec_path,"view","-bS",bowtie_file]
+			bam_file_path = os.path.join(output,bowtie_file) +".bam"
+			p = subprocess.Popen(cmd,stdoud=open(bam_file_path),stderr=subprocess.PIPE)
+
+		logger.debug("create_bam: changing dir: %s"%bowtie_path)
+		logger.debug("create_bam:END")
+		os.chdir(currentLocation)
+
+
