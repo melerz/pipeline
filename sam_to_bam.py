c0ce6af020d551b91878768e1678a57c4d0087e3
Guy Meler <melguy@gmail.com>
2015-05-22 10:30:45 -0400

    comment argparse code

--- sam_to_bam.py
+++ sam_to_bam.py
@@ -70,7 +70,7 @@ def create_bam(bowtie_path,output="./bam_files/",exec_path="/cs/wetlab/pipeline/
 					print "Error while executing samtools sort:",err #output goes to file
 				else:
 					#Delete the original BAM file
-			        logger.debug("Remove original BAM file: %s"%(bam_file_path))
+					logger.debug("Remove original BAM file: %s"%(bam_file_path))
 					os.remove(bam_file_path)
 
 		logger.debug("create_bam: changing dir: %s"%currentLocation)
