0b010c3371d9226ebe9e3bff1192bf3f19fcc24b
Guy Meler <melguy@gmail.com>
2015-06-25 11:24:34 -0400

    Support for unified hub

--- pipeline.py
+++ pipeline.py
@@ -44,7 +44,7 @@ def run(name,csv,illumina_name,workflow,configuration=None,log=None,force=False,
 
 	try:
 		if not log:
-			log = logging.getLogger(__name__)
+			global log = logging.getLogger(__name__)
 		workflow = config['workflows'][workflow]
 
 		experiment_working_directory = funcs.get_working_directory(name)#Function in configuration.config
