f666f130d0e7f4eb9749f3df8b2f896d88ad779e
Guy Meler <melguy@gmail.com>
2015-07-16 06:53:27 -0400

    Add documentation

--- lib/configuration/config.py
+++ lib/configuration/config.py
@@ -1,5 +1,7 @@
 '''
-This module loads the configuration file
+	This module provides the following:
+		1) Loads the configuration file - config.json
+		2) Initialize global functions for the pipeline scripts
 '''
 
 from .. import *
@@ -18,12 +20,33 @@ config=json.load(open(configuration_path))
 SAMPLE_DIR_FORMAT="sample-"
 
 def build_profile_dir_path(name):
+	'''
+		Builds the experiment folder *path* inside the ~/www folder of the current user.
+		This function is an helper function to get_working_directory function
+
+		Args:
+			- name: The experiment name
+		Returns:
+			The path for the main experiment dir
+	'''
+
 	www_path = expanduser("~/www")
-	#experiment_dir = os.path.join(www_path,name)
-	experiment_dir="/cs/wetlab/melerz/website/60-maayan/"
+	experiment_dir = os.path.join(www_path,name)
+	#experiment_dir="/cs/wetlab/melerz/website/60-maayan/"
 	return experiment_dir	
 
 def build_cs_dir_path(name):
+	'''
+		Builds the experiment folder *path* inside the WORKING_DIR config setting.
+		The folder from (build_profile_dir_path) is linked to this folder.
+		In this way, we avoid quota issues.
+		This function is an helper function to get_working_directory function
+
+		Args:
+			- name: The experiment name
+		Returns:
+			The path for the experiment dir inside the WORKING_DIR config setting
+	'''
 	username = getpass.getuser()
 	current_datetime = time.strftime("%d-%m-%y-%H-%M")
 	#Creating relevant experiment dir in working directory
@@ -32,6 +55,15 @@ def build_cs_dir_path(name):
 	return experiment_wd_dir
 
 def build_sample_dir_path(sample_name):
+	'''
+		Builds the sample dir inside the experiment dir.
+		This function is an helper function to get_working_directory function.
+
+		Args:
+			- sample_name: The current sample name of the pipeline
+		Returns:
+			The path for the current sample name
+	'''
 	path=SAMPLE_DIR_FORMAT+sample_name
 	return path
 
@@ -79,8 +111,13 @@ def get_working_directory(name,sample_name=None):
 	#If sample_name is not specified, return the experiment dir path only	
 	return experiment_dir
 
-
 def get_samples(name):
+	'''
+		Args:
+			- name - The experiment name
+		Returns:
+			Returns a list of all the sample dirs inside the experiment folder
+	'''
 	working_directory = get_working_directory(name)
 	sample_dirs = glob.glob(working_directory+"/"+SAMPLE_DIR_FORMAT+"*")
 	return sample_dirs
