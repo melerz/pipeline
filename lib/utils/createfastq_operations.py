b9c36b34f43a4420297083da96c667d3aa066371
Guy Meler <melguy@gmail.com>
2016-04-14 05:22:51 -0400

    fix borken url

--- lib/utils/createfastq_operations.py
+++ lib/utils/createfastq_operations.py
@@ -57,7 +57,9 @@ def runExpirement(xml_configuration,samplesheet,xml_path="./RunInfo.xml"):
 		createSampleSheet(samplesheet,"./SampleSheet.csv")
 
 		#run the bcl2fastq
-		p = subprocess.Popen(["/usr/local/bcl2fastq/2.15.0.4/bin/bcl2fastq","-o","fastq","-p","8","-d","6","-r","4","-w","4"],
+
+		bcl2fastq_exec = config['tools']['bcl2fastq']['exec']
+		p = subprocess.Popen([bcl2fastq_exec,"-o","fastq","-p","8","-d","6","-r","4","-w","4"],
 									stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 		#Blocking?
 		out,err=p.communicate() #Blocking...
